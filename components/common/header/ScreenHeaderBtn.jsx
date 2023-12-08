0722872f9b11d900b251c9b4161ceec2e24be9e3
ayhamShaker <ayham.shaker@pandiv.com>
2023-12-08 16:34:29 +0200

    asd
    asdasd

---
+++ components/common/header/ScreenHeaderBtn.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { TouchableOpacity, Image } from "react-native";
+
+import styles from "./screenheader.style";
+import { Images } from "../../../constants";
+
+const ScreenHeaderBtn = ({ iconUrl, dimension, handlePress }) => {
+  console.log("icon", icons.menu);
+  return (
+    <TouchableOpacity style={styles.btnContainer} onPress={handlePress}>
+      <Image
+        source={iconUrl}
+        resizeMode="cover"
+        style={styles.btnImg(dimension)}
+      />
+    </TouchableOpacity>
+  );
+};
+
+export default ScreenHeaderBtn;
---
+++ constants/images.js
@@ -0,0 +1,6 @@
+import React from "react";
+import profile from "../assets/images/kemal.jpg";
+
+export default {
+  profile,
+};
